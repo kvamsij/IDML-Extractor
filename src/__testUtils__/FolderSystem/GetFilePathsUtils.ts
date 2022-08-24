05d6334f6d54fcf03869473478fc434c05092a05
Vamsi Kalpamanu <kvamsij@gmail.com>
2022-08-24 18:23:15 +0100

    fix(getfilepathsutils.ts): if env values are not defined then use tmpdir for testing

--- src/__testUtils__/FolderSystem/GetFilePathsUtils.ts
+++ src/__testUtils__/FolderSystem/GetFilePathsUtils.ts
@@ -1,5 +1,7 @@
+import { tmpdir } from 'os';
+
 export function GetFilePathsUtils() {
-  const rootBucket = process.env.ROOT_BUCKET ? process.env.ROOT_BUCKET : 'nothing';
+  const rootBucket = process.env.ROOT_BUCKET ? process.env.ROOT_BUCKET : tmpdir();
   const idmlFileBucket = process.env.IDML_FILE_BUCKET ? process.env.IDML_FILE_BUCKET : 'nothing';
   const zipFileBucket = process.env.ZIP_FILE_BUCKET ? process.env.ZIP_FILE_BUCKET : 'nothing';
   const unZipFileBucket = process.env.UNZIP_FILE_BUCKET ? process.env.UNZIP_FILE_BUCKET : 'nothing';

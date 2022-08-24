75185162b5c727750cb36375f28ca02ba280e0e2
Vamsi Kalpamanu <kvamsij@gmail.com>
2022-08-24 12:41:47 +0100

    feat(ifilecopier.ts): change method infer type to Promise<string>
    re #48

--- src/libs/FileCopier/IFileCopier.ts
+++ src/libs/FileCopier/IFileCopier.ts
@@ -1,7 +1,3 @@
-import { IDMLExtractorError } from '../CustomError/IDMLExtractorError';
-
-export type Response = (IDMLExtractorError | null)[] | (string | null)[];
-
 export interface IFileCopier {
-  copy(): Promise<Response>;
+  copy(): Promise<string>;
 }

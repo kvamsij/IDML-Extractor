c993a3db4c2f81669667a25373c42d3b1f79d400
Vamsi Kalpamanu <kvamsij@gmail.com>
2022-08-24 21:07:57 +0100

    fix(configsetuputils, getfilepathsutils): rename files and folders

--- src/__testUtils__/FolderSystem/GetFilePathsUtils.ts
+++ src/__testUtils__/FolderSystem/GetFilePathsUtils.ts
@@ -1,7 +1,10 @@
+import { tmpdir } from 'os';
+
 export function GetFilePathsUtils() {
-  const rootBucket = process.env.ROOT_BUCKET ?? 'IDMLExtractorFileSystem';
-  const idmlFileBucket = process.env.IDML_FILE_BUCKET ?? 'idml-files';
+  const location = process.env.NODE_DEV === 'true' ? process.cwd() : tmpdir();
+  const rootBucket = process.env.ROOT_BUCKET ?? 'rootBucket';
+  const idmlFileBucket = process.env.IDML_FILE_BUCKET ?? 'idmls-files';
   const zipFileBucket = process.env.ZIP_FILE_BUCKET ?? 'zip-files';
-  const unZipFileBucket = process.env.UNZIP_FILE_BUCKET ?? 'unzip-files';
-  return { rootBucket, idmlFileBucket, zipFileBucket, unZipFileBucket };
+  const unzipFileBucket = process.env.UNZIP_FILE_BUCKET ?? 'unzip-files';
+  return { location, rootBucket, idmlFileBucket, zipFileBucket, unzipFileBucket };
 }

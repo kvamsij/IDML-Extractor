9e4d75ede023726785007f4b0a2955ffcb8ef984
Vamsi Kalpamanu <kvamsij@gmail.com>
2022-08-26 19:42:39 +0100

    refactor(errorhandler test suit): errorhandler.spec.ts
    refactored errorhandler.spec.ts to use setup.ts for test set-up and clean-up

--- src/__tests__/ErrorHandler/ErrorHandler.spec.ts
+++ src/__tests__/ErrorHandler/ErrorHandler.spec.ts
@@ -1,14 +1,31 @@
 import { ErrorHandler } from '@src/libs/ErrorHandler/ErrorHandler';
-import { ErrorHandlerTestCleanUp } from '@src/__testUtils__/ErrorHandler/ErrorHandlerTestCleanUp';
-import { ErrorHandlerTestSetUp } from '@src/__testUtils__/ErrorHandler/ErrorHandlerTestSetUp';
-import { filePaths, testFolder } from '@src/__testUtils__/ErrorHandler/filePaths';
+import { ErrorHandlerFilePaths } from '@src/libs/ErrorHandler/IErrorHandler';
+import { FolderSystemFilePaths } from '@src/libs/FolderSystem/IFolderSystem';
+import { CleanUp, CreateFolders, GetFilePaths } from '@src/__testUtils__/setUp';
 import { existsSync } from 'fs';
 
-beforeAll(ErrorHandlerTestSetUp(testFolder, filePaths));
-afterAll(ErrorHandlerTestCleanUp(testFolder));
+const filename = 'fake';
+let filePaths: FolderSystemFilePaths;
+let errorHandlerFilePaths: ErrorHandlerFilePaths;
+
+beforeAll(async () => {
+  await CreateFolders(filename);
+  filePaths = GetFilePaths(filename);
+});
+
 afterEach(() => jest.clearAllMocks());
+afterAll(async () => {
+  await CleanUp();
+});
 
 describe('ErrorHandler', () => {
+  beforeEach(() => {
+    errorHandlerFilePaths = {
+      zipFilePath: filePaths.fileRenameFilePaths.sourcePath,
+      idmlFilePath: filePaths.fileCopierFilePaths.sourcePath,
+      extractedFolderPath: filePaths.zipExtractorFilePaths.destinationPath,
+    };
+  });
   ErrorHandlerInstantiationTest();
   ErrorHandlerImplementationTest();
   ErrorHandlerErrorChecksTest();
@@ -16,36 +33,40 @@ describe('ErrorHandler', () => {
 
 function ErrorHandlerInstantiationTest() {
   describe('ErrorHandler: Instantiation', () => {
-    const errorHandler = new ErrorHandler(filePaths);
     it('should be able to create new(filePaths) on class ErrorHandler', () => {
+      const errorHandler = new ErrorHandler(errorHandlerFilePaths);
       expect(errorHandler).toBeTruthy();
     });
     it('should be called with filePaths', () => {
-      expect(errorHandler).toMatchObject({ filePaths });
+      const errorHandler = new ErrorHandler(errorHandlerFilePaths);
+      expect(errorHandler).toMatchObject({ filePaths: errorHandlerFilePaths });
     });
   });
 }
 
 function ErrorHandlerImplementationTest() {
   describe('ErrorHandler: Implementation', () => {
-    const errorHandler = new ErrorHandler(filePaths);
-    const spyOnHandleMock = jest.spyOn(errorHandler, 'handle');
-
     it('should call handler() once', () => {
+      const errorHandler = new ErrorHandler(errorHandlerFilePaths);
+      const spyOnHandleMock = jest.spyOn(errorHandler, 'handle');
       errorHandler.handle();
       expect(spyOnHandleMock).toBeCalledTimes(1);
     });
 
     it('should delete zipFile if exists', () => {
-      const isZipFileExists = existsSync(filePaths.zipFilePath);
+      const errorHandler = new ErrorHandler(errorHandlerFilePaths);
+      errorHandler.handle();
+      const isZipFileExists = existsSync(errorHandlerFilePaths.zipFilePath);
       expect(isZipFileExists).toBeFalsy();
     });
     it('should delete idmlFile if exists', () => {
-      const isIdmlFileExists = existsSync(filePaths.idmlFilePath);
+      const errorHandler = new ErrorHandler(errorHandlerFilePaths);
+      errorHandler.handle();
+      const isIdmlFileExists = existsSync(errorHandlerFilePaths.idmlFilePath);
       expect(isIdmlFileExists).toBeFalsy();
     });
     it('should delete extractedFolder if exists', () => {
-      const isFolderExists = existsSync(filePaths.extractedFolderPath);
+      const isFolderExists = existsSync(errorHandlerFilePaths.extractedFolderPath);
       expect(isFolderExists).toBeFalsy();
     });
   });

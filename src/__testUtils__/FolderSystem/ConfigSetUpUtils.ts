77e53546c5bda36a8867c96db4da80da6367409f
Vamsi Kalpamanu <kvamsij@gmail.com>
2022-08-26 22:49:27 +0100

    refactor: delete unnecessary files

--- src/__testUtils__/FolderSystem/ConfigSetUpUtils.ts
+++
@@ -1,7 +0,0 @@
-export function ConfigSetUpUtils() {
-  const rootBucket = process.env.ROOT_BUCKET ?? 'rootBucket';
-  const idmlFileBucket = process.env.IDML_FILE_BUCKET ?? 'idmls-files';
-  const zipFileBucket = process.env.ZIP_FILE_BUCKET ?? 'zip-files';
-  const unZipFileBucket = process.env.UNZIP_FILE_BUCKET ?? 'unzip-files';
-  return { rootBucket, idmlFileBucket, zipFileBucket, unZipFileBucket };
-}

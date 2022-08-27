aeb97fa2773849adf39703a5aa981c4f6742a530
Vamsi Kalpamanu <kvamsij@gmail.com>
2022-08-27 10:25:12 +0100

    refactor(defaultconfig.ts): change config type

--- src/libs/Config/DefaultConfig.ts
+++ src/libs/Config/DefaultConfig.ts
@@ -1,17 +1,17 @@
 import { tmpdir } from 'os';
 import dotenv from 'dotenv';
 
-export type DefaultConfigProperties = {
-  location: string | undefined;
-  rootBucket: string | undefined;
-  idmlFileBucket: string | undefined;
-  zipFileBucket: string | undefined;
-  unzipFileBucket: string | undefined;
+export type ConfigProperties = {
+  location: string;
+  rootBucket: string;
+  idmlFileBucket: string;
+  zipFileBucket: string;
+  unzipFileBucket: string;
 };
 dotenv.config();
 
 export class DefaultConfig {
-  private defaultConfigProperties: DefaultConfigProperties = {
+  private defaultConfigProperties: ConfigProperties = {
     location: process.env.NODE_DEV === 'true' ? tmpdir() : process.cwd(),
     rootBucket: 'IDMLExtractorFileSystem',
     idmlFileBucket: 'idmls-files',
@@ -19,12 +19,12 @@ export class DefaultConfig {
     unzipFileBucket: 'unzip-files',
   };
 
-  private envConfigProperties: DefaultConfigProperties = {
+  private envConfigProperties: ConfigProperties = {
     location: process.env.NODE_DEV === 'true' ? tmpdir() : process.cwd(),
-    rootBucket: process.env.ROOT_BUCKET,
-    idmlFileBucket: process.env.IDML_FILE_BUCKET,
-    zipFileBucket: process.env.ZIP_FILE_BUCKET,
-    unzipFileBucket: process.env.UNZIP_FILE_BUCKET,
+    rootBucket: process.env.ROOT_BUCKET ?? '',
+    idmlFileBucket: process.env.IDML_FILE_BUCKET ?? '',
+    zipFileBucket: process.env.ZIP_FILE_BUCKET ?? '',
+    unzipFileBucket: process.env.UNZIP_FILE_BUCKET ?? '',
   };
 
   getConfigProperties() {
